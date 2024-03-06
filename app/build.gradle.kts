fd910821238e3603bdaed3a7d8b4b8ab470eb873
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-03-06 11:02:35 -0500

    Updated gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.7")
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.google.firebase:firebase-database")
 
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
