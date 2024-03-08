d35c9c46fcfb8fddd23c4806e9c47e3be0fa6ada
mandeepSidhuNeu <sidhu.m@northeastern.edu>
2024-03-07 23:13:46 -0500

    add models for post and comment

--- app/src/main/java/com/example/unisphere/model/Post.java
+++ app/src/main/java/com/example/unisphere/model/Post.java
@@ -11,4 +11,44 @@ public class Post {
     private String userId;
     private Long likes;
     private List<Comment> comments;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Long likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
