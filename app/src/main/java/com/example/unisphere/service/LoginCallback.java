66202615924ce09815b900224317d391b9c7461c
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-03-10 12:12:44 -0400

    Added signup page UI

--- app/src/main/java/com/example/unisphere/service/LoginCallback.java
+++ app/src/main/java/com/example/unisphere/service/LoginCallback.java
@@ -9,13 +9,13 @@ public interface LoginCallback {
 
     /**
      * Callback function called on successful login.
-     * @param user user that is logged in.
+     * @param email that is logged in.
      */
-    public void onLoginSuccess(String email);
+     void onLoginSuccess(String email);
 
     /**
      * Callback function called in case login is unsuccessful
      * @param e exception during login
      */
-    public void onLoginFailure(Exception e);
+     void onLoginFailure(Exception e);
 }
