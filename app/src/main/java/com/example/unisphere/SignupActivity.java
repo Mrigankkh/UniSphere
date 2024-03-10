49e18394371775176da814638c5d14bd8c201a9a
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-03-09 22:24:16 -0500

    Added code for sign up page

--- app/src/main/java/com/example/unisphere/SignupActivity.java
+++ app/src/main/java/com/example/unisphere/SignupActivity.java
@@ -1,14 +1,21 @@
 package com.example.unisphere;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
+
 }
