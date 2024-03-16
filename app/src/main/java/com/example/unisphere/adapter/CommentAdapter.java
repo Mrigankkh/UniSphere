557c256565d92a8614a83f87cac4cfda12c0d2ca
mandeepSidhuNeu <sidhu.m@northeastern.edu>
2024-03-16 17:57:59 -0400

    create adapter and holder for comment

--- app/src/main/java/com/example/unisphere/adapter/CommentAdapter.java
+++ app/src/main/java/com/example/unisphere/adapter/CommentAdapter.java
@@ -1,2 +1,42 @@
-package com.example.unisphere.adapter;public class CommentAdapter {
+package com.example.unisphere.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.unisphere.R;
+import com.example.unisphere.model.Comment;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentViewHolder> {
+
+    private List<Comment> commentList;
+
+    public CommentAdapter(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_item, parent, false);
+        return new CommentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+        Comment comment = commentList.get(position);
+        holder.bind(comment);
+    }
+
+    @Override
+    public int getItemCount() {
+        return commentList.size();
+    }
+
 }
--- app/src/main/java/com/example/unisphere/adapter/CommentViewHolder.java
+++ app/src/main/java/com/example/unisphere/adapter/CommentViewHolder.java
@@ -1,2 +1,28 @@
-package com.example.unisphere.adapter;public class CommentViewHolder {
+package com.example.unisphere.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.unisphere.R;
+import com.example.unisphere.model.Comment;
+
+public class CommentViewHolder  extends RecyclerView.ViewHolder {
+    TextView textViewComment;
+    TextView textViewEmail;
+
+
+    CommentViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textViewComment = itemView.findViewById(R.id.textView_comment);
+        textViewEmail = itemView.findViewById(R.id.textView_email);
+    }
+
+    void bind(Comment comment) {
+        textViewComment.setText(comment.getText());
+        textViewEmail.setText(comment.getUserId());
+
+    }
 }
