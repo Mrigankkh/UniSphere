fff51e5aab0dbbe7822af7141d747c96362b8dff
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-03-26 12:52:01 -0400

    Added tag view item layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
 
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
