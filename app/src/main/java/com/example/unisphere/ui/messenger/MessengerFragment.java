1c99b8aa2a526ea6a60d1ce6b7d37a6024da83c7
Ayush Desai <aayushdesai88@gmail.com>
2024-03-28 14:54:12 -0400

    Search bar for the page implemented with recycler view, just need to check in the uni details

--- app/src/main/java/com/example/unisphere/ui/messenger/MessengerFragment.java
+++ app/src/main/java/com/example/unisphere/ui/messenger/MessengerFragment.java
@@ -1,66 +1,132 @@
 package com.example.unisphere.ui.messenger;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.unisphere.R;
+import com.example.unisphere.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessengerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MessengerFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText searchBar;
+    private RecyclerView usersRecyclerView;
+    private DatabaseReference usersDatabaseReference;
+    private List<User> userList = new ArrayList<>();
 
     public MessengerFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessengerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MessengerFragment newInstance(String param1, String param2) {
         MessengerFragment fragment = new MessengerFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messenger, container, false);
+        searchBar = view.findViewById(R.id.search_bar);
+        usersRecyclerView = view.findViewById(R.id.search_results_recycler_view);
+        usersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        final UserSearchInnerAdapter userSearchAdapter = new UserSearchInnerAdapter(userList);
+        usersRecyclerView.setAdapter(userSearchAdapter);
+        usersDatabaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String searchText = s.toString().trim();
+                if (!searchText.isEmpty()) {
+                    performSearch(searchText, userSearchAdapter);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messenger, container, false);
+    private void performSearch(String searchText, final UserSearchInnerAdapter adapter) {
+        Query searchQuery = usersDatabaseReference.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+        searchQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    userList.add(user);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private static class UserSearchInnerAdapter extends RecyclerView.Adapter<UserSearchInnerAdapter.UserViewHolder> {
+
+        private List<User> userList;
+
+        public UserSearchInnerAdapter(List<User> userList) {
+            this.userList = userList;
+        }
+
+        @NonNull
+        @Override
+        public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
+            return new UserViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+            User user = userList.get(position);
+            holder.userNameTextView.setText(user.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return userList.size();
+        }
+
+        static class UserViewHolder extends RecyclerView.ViewHolder {
+
+            TextView userNameTextView;
+
+            UserViewHolder(View itemView) {
+                super(itemView);
+                userNameTextView = itemView.findViewById(R.id.user_name_text_view);
+            }
+        }
     }
 }
